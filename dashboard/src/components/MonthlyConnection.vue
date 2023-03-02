d8c5bc86acb1141438f13ae7bb9fa7d6644788b6
MaTtiaa01 <mattia2001@live.it>
2023-03-02 11:24:28 +0100

    add line chart with monthly connections

--- dashboard/src/components/MonthlyConnection.vue
+++ dashboard/src/components/MonthlyConnection.vue
@@ -8,36 +8,46 @@ export default {
     data() {
         return {
             store,
-            xValues: [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150],
-            yValues: [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15],
         }
     },
     methods: {
         newLineChart() {
-            let myChart = new Chart("myChart", {
+            const data = []
+            for (let index = 0; index < this.store.monthlyConnections.length; index++) {
+                const element = this.store.monthlyConnections[index];
+                //console.log(element)
+                data.push({ x: element.month, y: element.connections })
+                console.log(data);
+            }
+
+            new Chart(document.getElementById("myChart"), {
                 type: "line",
                 data: {
-                    labels: this.xValues,
                     datasets: [{
-                        backgroundColor: "rgba(0,0,0,1.0)",
-                        borderColor: "rgba(0,0,0,0.1)",
-                        data: this.yValues
+                        label: 'Monthly Connections',
+                        data: data,
+                        fill: false,
+                        borderColor: 'rgb(75, 192, 192)',
+                        tension: 0.1
                     }]
-                },
-                options: {}
+                }
             })
 
+        },
+        wait(fun) {
+            setTimeout(fun, 3000)
         }
     },
     mounted() {
         store.getData()
+        this.wait(this.newLineChart)
     }
 }
 </script>
 
 <template>
     <div class="container mb-3 d-flex justify-content-center">
-        <canvas id="myChart" style="width:100%;max-width:800px"></canvas>
+        <canvas id="myChart" style="width:100%;max-width:800px;"></canvas>
     </div>
 </template>
 
--- dashboard/src/store.js
+++ dashboard/src/store.js
@@ -2,16 +2,21 @@ import { reactive } from "vue";
 import axios from 'axios';
 
 export const store = reactive({
-    myData: null,
+    devices: null,
+    monthlyConnections: null,
+    userAgeRange: null,
     api_url: 'https://aff904b2-0df3-444b-91c7-76d17288c169.mock.pstmn.io/data',
     getData() {
         axios
             .get(this.api_url)
-            .then(response => {
-                console.log(response)
-                this.myData = response.data
+            .then((response) => {
+                //console.log(response.data)
+                this.devices = response.data.Devices
+                this.monthlyConnections = response.data.MonthlyConnections
+                this.userAgeRange = response.data.UserAgeRange
+                console.log(this.monthlyConnections);
             })
-            .catch(error => {
+            .catch((error) => {
                 console.log(error.message)
                 this.error = error.message
                 this.loading = false
